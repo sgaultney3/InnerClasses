f43705efae08e99cf3ab5d3d7eb9a63bdaa7e41d
sgaultney3 <samuel.gaultney@gmail.com>
2017-03-27 15:48:04 -0400

    Initial commit

---
+++ src/com/SamuelGaultney/Button.java
@@ -0,0 +1,25 @@
+package com.SamuelGaultney;
+
+/**
+ * Created by Samuel on 3/27/2017.
+ */
+public class Button {
+    private String title;
+    private OnClickListener onClickListener;
+
+    public Button(String title) {
+        this.title = title;
+    }
+
+    public void setOnClickListener(OnClickListener onClickListener) {
+        this.onClickListener = onClickListener;
+    }
+
+    public void onClick() {
+        this.onClickListener.onClick(this.title);
+    }
+
+    public interface OnClickListener {
+        public void onClick(String title);
+    }
+}
